9c08b298b411ed04645fd52f1a98ac7b6444a735
Anish Karandikar <anishkny@gmail.com>
2021-09-03 09:14:19 -0700

    Add index.js

---
+++ index.js
@@ -0,0 +1,42 @@
+const fs = require("fs");
+const path = require("path");
+const crypto = require("crypto");
+const baseTest = require("@playwright/test").test;
+
+const istanbulCLIOutput = path.join(process.cwd(), ".nyc_output");
+
+function generateUUID() {
+  return crypto.randomBytes(16).toString("hex");
+}
+
+const test = baseTest.extend({
+  context: async ({ context }, use) => {
+    await context.addInitScript(() =>
+      window.addEventListener("beforeunload", () =>
+        window.collectIstanbulCoverage(JSON.stringify(window.__coverage__))
+      )
+    );
+    await fs.promises.mkdir(istanbulCLIOutput, { recursive: true });
+    await context.exposeFunction("collectIstanbulCoverage", (coverageJSON) => {
+      if (coverageJSON)
+        fs.writeFileSync(
+          path.join(
+            istanbulCLIOutput,
+            `playwright_coverage_${generateUUID()}.json`
+          ),
+          coverageJSON
+        );
+    });
+    await use(context);
+    for (const page of context.pages()) {
+      await page.evaluate(() =>
+        window.collectIstanbulCoverage(JSON.stringify(window.__coverage__))
+      );
+      await page.close();
+    }
+  },
+});
+
+const expect = test.expect;
+
+module.exports = { test, expect };
