b8571a3fcce11ba6f4c335187ed1991af564d81a
Anish Karandikar <anishkny@gmail.com>
2022-04-27 17:08:01 -0700

    chore(ci): Release dry run and configure standard version (#43)
    * chore(ci): Perform release dry run
    * chore: Configure standard version

---
+++ .versionrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  types: [
+    { type: "feat", section: "Features" },
+    { type: "fix", section: "Bug Fixes" },
+    { type: "chore", section: "Chores" },
+    { type: "docs", section: "Docs" },
+    { type: "test", section: "Test" },
+  ],
+};
