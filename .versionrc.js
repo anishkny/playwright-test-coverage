e8589cd1a40608a3f7510d8dcb0218fc2f856a0b
Anish Karandikar <anishkny@gmail.com>
2023-03-21 19:32:31 -0700

    chore(version): Add link to unreleased features in CHANGELOG

--- .versionrc.js
+++ .versionrc.js
@@ -1,4 +1,11 @@
 module.exports = {
+  header: `# Changelog
+
+All notable changes to this project will be documented in this file.
+See [commit-and-tag-version](https://github.com/absolute-version/commit-and-tag-version) for commit guidelines.
+
+### [Unreleased](https://github.com/anishkny/playwright-test-coverage/commits/main) (if any)
+`,
   scripts: {
     // Update version number in test/package-lock.json
     postbump: "npm run pack && git add test/package-lock.json",
